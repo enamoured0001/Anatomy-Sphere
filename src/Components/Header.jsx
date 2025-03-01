70c1d3cd481233e58a72a685133d8cb4398307d7
mohitpandey <mohitpandey9315339061@gmail.com>
2025-03-01 20:34:10 +0530

    Update media

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,10 +9,10 @@ function Header() {
     
   return (
     <header className=" bg-gray-300 p-1 flex justify-between w-[98.7vw] font-black  shadow-xs  items-center">
-         <div className='flex  gap-x-2 lg:pl-20  pt-2 pb-2'>
+         <div className='flex  gap-x-2 lg:pl-20   pt-2 pb-2'>
             <img  className='flex    h-10 w-10 rounded-full' src={Logo} alt="" />
             </div>
-        <h6 className='font-bold lg:pl-48  md:pl-40 text-xl'>  Anatomy  <ReactTyped
+        <h6 className='font-bold lg:pl-48 pl-20 md:pl-40 text-xl'>  Anatomy  <ReactTyped
                  className='font-bold pl-3 text-xl text-blue-600'
           strings={['Prism', 'Learning', 'Sphere']}
           typeSpeed={40}
@@ -20,7 +20,7 @@ function Header() {
         
           />  </h6>
       <nav>
-        <ul className="flex lg:pr-20 space-x-4">
+        <ul className="flex lg:pr-20 hidden lg:flex space-x-4">
           <li>
             <button  className="text-gray-800 hover:text-blue-700">Home</button>
           </li>
